05b7f1c06adb227a76c7a3167564fff7ce74299b
Wouter Gritter <woutergritter@gmail.com>
2020-08-27 18:47:26 +0200

    add easy way to send message using LangConfig

--- src/main/java/me/woutergritter/plugintemplate/config/LangConfig.java
+++ src/main/java/me/woutergritter/plugintemplate/config/LangConfig.java
@@ -2,6 +2,7 @@ package me.woutergritter.plugintemplate.config;
 
 import me.woutergritter.plugintemplate.Main;
 import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
 
 import java.util.Locale;
 
@@ -15,5 +16,9 @@ public class LangConfig extends Config {
         msg = ChatColor.translateAlternateColorCodes('&', msg);
 
         return String.format(Locale.ENGLISH, msg, args);
+    }
+
+    public void sendMessage(CommandSender player, String path, Object... args) {
+        player.sendMessage(getMessage(path, args));
     }
 }
