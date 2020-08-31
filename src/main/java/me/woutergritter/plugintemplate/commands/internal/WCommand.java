e4ed80c6f0dbafd2c4cc67b869745ca2e91daafd
Wouter Gritter <woutergritter@gmail.com>
2020-08-31 13:23:41 +0200

    use Command instead of CommandExecutor to avoid writing down commands in plugin.yml

--- src/main/java/me/woutergritter/plugintemplate/commands/internal/WCommand.java
+++ src/main/java/me/woutergritter/plugintemplate/commands/internal/WCommand.java
@@ -1,22 +1,27 @@
 package me.woutergritter.plugintemplate.commands.internal;
 
 import me.woutergritter.plugintemplate.Main;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandMap;
 import org.bukkit.command.CommandSender;
 
-public abstract class WCommand implements CommandExecutor {
+import java.lang.reflect.Field;
+
+public abstract class WCommand extends Command {
     protected final Main plugin;
     protected final String command;
 
     public WCommand(Main plugin, String command) {
+        super(command);
         this.plugin = plugin;
         this.command = command;
     }
 
     public abstract void execute(CommandContext ctx);
 
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    @Override
+    public final boolean execute(CommandSender sender, String label, String[] args) {
         CommandContext ctx = new CommandContext(plugin, this, sender, args);
 
         try{
@@ -29,10 +34,26 @@ public abstract class WCommand implements CommandExecutor {
     }
 
     public void register() {
-        plugin.getCommand(this.command).setExecutor(this);
+        CommandMap commandMap = getCommandMap();
+        if(commandMap != null) {
+            commandMap.register(command, this);
+        }
     }
 
     public String getCommand() {
         return command;
+    }
+
+    private static CommandMap getCommandMap() {
+        try{
+            Field commandMapField = Bukkit.getServer().getClass().getDeclaredField("commandMap");
+            commandMapField.setAccessible(true);
+
+            return (CommandMap) commandMapField.get(Bukkit.getServer());
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
